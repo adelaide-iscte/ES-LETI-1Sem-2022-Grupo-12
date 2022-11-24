0a2cea60af731d80ff36628a9929dce97fe87255
aadoa <aadoa@iscte-iul.pt>
2022-11-24 14:12:31 +0000

    alteração do horario, criaçao de variaveis para por no horario criaçao de uma funçao que ve cada bloco singularmente

--- Projeto_ES/src/main/java/Grupo12/Projeto_ES/Html.java
+++ Projeto_ES/src/main/java/Grupo12/Projeto_ES/Html.java
@@ -2,6 +2,9 @@ package Grupo12.Projeto_ES;
 
 import java.io.File;
 import java.io.PrintWriter;
+
+import org.json.JSONObject;
+
 import java.awt.Desktop; 
 import java.io.FileNotFoundException;
 
@@ -11,11 +14,15 @@ public class Html {
 		File file = new File("agenda.html");
 		try {
         String hora = "8:00-9:30";
+        int hour = 8;
         String professor = "fixolas";
         String sala = "auditorio";
         String disciplina = "LeagueOfLegends";        
-		int semana = 1;		
+		int semana = 1;	
 		
+		String bloco ="<td align=\"center\" height=\"100\"><font color=\"blue\">Cadeira: </font>"
+				+ "Engenharia Software<br><font color=\"blue\">Sala:</font> "
+				+ "B206<br><font color=\"blue\">Professor:</font> Atum Tuna</td>\r\n";
 		PrintWriter printWriter = new PrintWriter(file);				
 		String html = "<!DOCTYPE html>\r\n"
 				+ "<html>\r\n"
@@ -51,30 +58,30 @@ public class Html {
 				+ "        </tr>\r\n"
 				+ "        <tr>\r\n"
 				+ "            <td align=\"center\" height=\"100\">\r\n"
-				+ "                <b>8:00-9:30</b></td>\r\n"
-				+ "            <td align=\"center\" height=\"100\">Cadeira:<br>Sala:<br>Professor:</td>\r\n"
-				+ "            <td align=\"center\" height=\"100\">Cadeira:<br>Sala:<br>Professor:</td>\r\n"
-				+ "            <td align=\"center\" height=\"100\">Cadeira:<br>Sala:<br>Professor:</td>\r\n"
-				+ "            <td align=\"center\" height=\"100\">Cadeira:<br>Sala:<br>Professor:</td>\r\n"
-				+ "            <td align=\"center\" height=\"100\">Cadeira:<br>Sala:<br>Professor:</td>\r\n"
+				+ "                <b>" + hour + ":00-" + (hour+=1) + ":30</b></td>\r\n"
+				+ bloco(hour, null)
+				+ bloco(hour, null)
+				+ bloco(hour, null)
+				+ bloco(hour, null)
+				+ bloco(hour, null)
 				+ "        </tr>\r\n"
 				+ "        <tr>\r\n"
 				+ "            <td align=\"center\" height=\"100\">\r\n"
 				+ "                <b>9:30-11:00</b></td>\r\n"
-				+ "            <td align=\"center\" height=\"100\">Cadeira:<br>Sala:<br>Professor:</td>\r\n"
-				+ "            <td align=\"center\" height=\"100\">Cadeira:<br>Sala:<br>Professor:</td>\r\n"
-				+ "            <td align=\"center\" height=\"100\">Cadeira:<br>Sala:<br>Professor:</td>\r\n"
-				+ "            <td align=\"center\" height=\"100\">Cadeira:<br>Sala:<br>Professor:</td>\r\n"
-				+ "            <td align=\"center\" height=\"100\">Cadeira:<br>Sala:<br>Professor:</td>\r\n"
+				+ bloco(hour, null)
+				+ bloco(hour, null)
+				+ bloco(hour, null)
+				+ bloco(hour, null)
+				+ bloco(hour, null)
 				+ "        </tr>\r\n"
 				+ "        <tr>\r\n"
 				+ "            <td align=\"center\" height=\"100\">\r\n"
 				+ "                <b>11:00-12:30</b></td>\r\n"
-				+ "            <td align=\"center\" height=\"100\">Cadeira:<br>Sala:<br>Professor:</td>\r\n"
-				+ "            <td align=\"center\" height=\"100\">Cadeira:<br>Sala:<br>Professor:</td>\r\n"
-				+ "            <td align=\"center\" height=\"100\">Cadeira:<br>Sala:<br>Professor:</td>\r\n"
-				+ "            <td align=\"center\" height=\"100\">Cadeira:<br>Sala:<br>Professor:</td>\r\n"
-				+ "            <td align=\"center\" height=\"100\">Cadeira:<br>Sala:<br>Professor:</td>\r\n"
+				+ bloco(hour, null)
+				+ bloco(hour, null)
+				+ bloco(hour, null)
+				+ bloco(hour, null)
+				+ bloco(hour, null)
 				+ "        </tr>\r\n"
 				+ "        <tr>\r\n"
 				+ "            <td align=\"center\" height=\"50\">\r\n"
@@ -88,29 +95,29 @@ public class Html {
 				+ "        <tr>\r\n"
 				+ "            <td align=\"center\" height=\"100\">\r\n"
 				+ "                <b>13:00-14:30</b></td>\r\n"
-				+ "            <td align=\"center\" height=\"100\">Cadeira:<br>Sala:<br>Professor:</td>\r\n"
-				+ "            <td align=\"center\" height=\"100\">Cadeira:<br>Sala:<br>Professor:</td>\r\n"
-				+ "            <td align=\"center\" height=\"100\">Cadeira:<br>Sala:<br>Professor:</td>\r\n"
-				+ "            <td align=\"center\" height=\"100\">Cadeira:<br>Sala:<br>Professor:</td>\r\n"
-				+ "            <td align=\"center\" height=\"100\">Cadeira:<br>Sala:<br>Professor:</td>\r\n"
+				+ bloco(hour, null)
+				+ bloco(hour, null)
+				+ bloco(hour, null)
+				+ bloco(hour, null)
+				+ bloco(hour, null)
 				+ "        </tr>\r\n"
 				+ "        <tr>\r\n"
 				+ "            <td align=\"center\" height=\"100\">\r\n"
 				+ "                <b>14:30-16:00</b></td>\r\n"
-				+ "            <td align=\"center\" height=\"100\">Cadeira:<br>Sala:<br>Professor:</td>\r\n"
-				+ "            <td align=\"center\" height=\"100\">Cadeira:<br>Sala:<br>Professor:</td>\r\n"
-				+ "            <td align=\"center\" height=\"100\">Cadeira:<br>Sala:<br>Professor:</td>\r\n"
-				+ "            <td align=\"center\" height=\"100\">Cadeira:<br>Sala:<br>Professor:</td>\r\n"
-				+ "            <td align=\"center\" height=\"100\">Cadeira:<br>Sala:<br>Professor:</td>\r\n"
+				+ bloco(hour, null)
+				+ bloco(hour, null)
+				+ bloco(hour, null)
+				+ bloco(hour, null)
+				+ bloco(hour, null)
 				+ "        </tr>\r\n"
 				+ "        <tr>\r\n"
 				+ "            <td align=\"center\" height=\"100\">\r\n"
 				+ "                <b>16:00-17:30</b></td>\r\n"
-				+ "            <td align=\"center\" height=\"100\">Cadeira:<br>Sala:<br>Professor:</td>\r\n"
-				+ "            <td align=\"center\" height=\"100\">Cadeira:<br>Sala:<br>Professor:</td>\r\n"
-				+ "            <td align=\"center\" height=\"100\">Cadeira:<br>Sala:<br>Professor:</td>\r\n"
-				+ "            <td align=\"center\" height=\"100\">Cadeira:<br>Sala:<br>Professor:</td>\r\n"
-				+ "            <td align=\"center\" height=\"100\">Cadeira:<br>Sala:<br>Professor:</td>\r\n"
+				+ bloco(hour, null)
+				+ bloco(hour, null)
+				+ bloco(hour, null)
+				+ bloco(hour, null)
+				+ bloco(hour, null)
 				+ "        </tr>\r\n"
 				+ "        <tr>\r\n"
 				+ "            <td align=\"center\" height=\"50\">\r\n"
@@ -124,29 +131,29 @@ public class Html {
 				+ "        <tr>\r\n"
 				+ "            <td align=\"center\" height=\"100\">\r\n"
 				+ "                <b>18:00-19:30</b></td>\r\n"
-				+ "            <td align=\"center\" height=\"100\">Cadeira:<br>Sala:<br>Professor:</td>\r\n"
-				+ "            <td align=\"center\" height=\"100\">Cadeira:<br>Sala:<br>Professor:</td>\r\n"
-				+ "            <td align=\"center\" height=\"100\">Cadeira:<br>Sala:<br>Professor:</td>\r\n"
-				+ "            <td align=\"center\" height=\"100\">Cadeira:<br>Sala:<br>Professor:</td>\r\n"
-				+ "            <td align=\"center\" height=\"100\">Cadeira:<br>Sala:<br>Professor:</td>\r\n"
+				+ bloco(hour, null)
+				+ bloco(hour, null)
+				+ bloco(hour, null)
+				+ bloco(hour, null)
+				+ bloco(hour, null)
 				+ "        </tr>\r\n"
 				+ "        <tr>\r\n"
 				+ "            <td align=\"center\" height=\"100\">\r\n"
 				+ "                <b>19:30-21:00</b></td>\r\n"
-				+ "            <td align=\"center\" height=\"100\">Cadeira:<br>Sala:<br>Professor:</td>\r\n"
-				+ "            <td align=\"center\" height=\"100\">Cadeira:<br>Sala:<br>Professor:</td>\r\n"
-				+ "            <td align=\"center\" height=\"100\">Cadeira:<br>Sala:<br>Professor:</td>\r\n"
-				+ "            <td align=\"center\" height=\"100\">Cadeira:<br>Sala:<br>Professor:</td>\r\n"
-				+ "            <td align=\"center\" height=\"100\">Cadeira:<br>Sala:<br>Professor:</td>\r\n"
+				+ bloco(hour, null)
+				+ bloco(hour, null)
+				+ bloco(hour, null)
+				+ bloco(hour, null)
+				+ bloco(hour, null)
 				+ "        </tr>\r\n"
 				+ "        <tr>\r\n"
 				+ "            <td align=\"center\" height=\"100\">\r\n"
 				+ "                <b>21:00-22:30</b></td>\r\n"
-				+ "            <td align=\"center\" height=\"100\">Cadeira:<br>Sala:<br>Professor:</td>\r\n"
-				+ "            <td align=\"center\" height=\"100\">Cadeira:<br>Sala:<br>Professor:</td>\r\n"
-				+ "            <td align=\"center\" height=\"100\">Cadeira:<br>Sala:<br>Professor:</td>\r\n"
-				+ "            <td align=\"center\" height=\"100\">Cadeira:<br>Sala:<br>Professor:</td>\r\n"
-				+ "            <td align=\"center\" height=\"100\">Cadeira:<br>Sala:<br>Professor:</td>\r\n"
+				+ bloco(hour, null)
+				+ bloco(hour, null)
+				+ bloco(hour, null)
+				+ bloco(hour, null)
+				+ bloco(hour, null)
 				+ "        </tr>\r\n"
 				+ "    </table>\r\n"
 				+ "</body>\r\n"
@@ -174,6 +181,21 @@ public class Html {
 	    	}
 	     file.delete();
 	}
+	
+	public static String bloco(int hour, JSONObject obj) {
+		
+		String hora = Integer.toString(hour);
+		
+		
+		return "<td align=\"center\" height=\"100\"><font color=\"blue\">Cadeira: </font>"
+				+ "Engenharia Software<br><font color=\"blue\">Sala:</font> "
+				+ "B206<br><font color=\"blue\">Professor:</font> Atum Tuna</td>\r\n";
+		
+//		return "<td align=\"center\" height=\"100\"><font color=\"blue\">Cadeira: </font>"
+//		+ ((((JSONObject) obj.get(hora)).get("Cadeira"))) + "<br><font color=\"blue\">Sala:</font> "
+//		+ "B206<br><font color=\"blue\">Professor:</font> Atum Tuna</td>\r\n";
+	}
+
 	
 }
 
