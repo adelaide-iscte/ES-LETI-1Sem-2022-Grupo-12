1679f20523c744409361100ca688e8fc1e89bb4a
jphjf <jphjf@iscte-iul.pt>
2022-11-24 15:48:47 +0000

    Inicio do objeto JSON v1

--- Projeto_ES/src/main/java/Grupo12/Projeto_ES/Calendar.java
+++ Projeto_ES/src/main/java/Grupo12/Projeto_ES/Calendar.java
@@ -16,6 +16,8 @@ import java.net.URLConnection;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import org.json.JSONArray;
@@ -48,137 +50,86 @@ public class Calendar {
 		return null;
 	}
 
-	public static void uriInformation(String name) {
 
-		StringBuilder content = new StringBuilder();
 
-//		try {
-//			URI uri = new URI(WebcalUri.split(":")[2]);
-//			URI newUri = new URI("http", uri.getHost(), uri.getPath(), uri.getQuery(), null);
-//			URL url = newUri.toURL();
-//			URLConnection urlConnection = url.openConnection();
-//			BufferedReader buffReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-//
-//			String line;
-//			while ((line=buffReader.readLine()) != null) {
-//				content.append(line + "\n");
-//			}
-//			
-//			buffReader.close();
-//			
-//			System.out.println(content.toString());
-//
-//		} catch (URISyntaxException e) {
-//			System.out.println(
-//					"Erro a formar o URI, certifique-se que ao adicionar o URI não deixou um espaço em branco no fim");
-//		} catch (MalformedURLException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
 
-		try {
-			URI uri = new URI(getURI(name).split(":")[2]);
-			URI newUri = new URI("http", uri.getHost(), uri.getPath(), uri.getQuery(), null);
-			URL url = newUri.toURL();
-
-			InputStream in = url.openStream();
-			Files.copy(in, Paths.get(name + "Calendario.txt"), StandardCopyOption.REPLACE_EXISTING);
-
-			System.out.println(url.toString());
-
-		} catch (URISyntaxException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-	}
-
-	public static void workWeek(String name, int week) {
-
-		File f = new File(name + "Calendario.txt");
-		int fullWeek = week;
-		JSONObject allDays = new JSONObject();
-
-		try {
-			Scanner sc = new Scanner(f);
-
-			while (fullWeek != (week + 5)) {
-				while (sc.hasNextLine()) {
-					String dtStart = sc.nextLine();
-					if (dtStart.split(":")[0].equals("DTSTART")) {
-						String[] start = dtStart.split("T");
-						String[] date = start[3].split(":");
-						if (date[1].equals(Integer.toString(fullWeek))) {
-							while (!sc.nextLine().equals("END:VEVENT")) {
-								System.out.println("It works");
-								sc.nextLine();
-							}
-						}
-
-					}
-				}
-
-				fullWeek++;
-			}
-
-			sc.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-
-	}
 	
-	public static void readDay (String string, int day){
+	public static List<String> readDay (String string, int day){
 
 		File file = new File(string);
-
 		
+		List<String> aulas = new ArrayList<String>();
+		
+
+
 		try { 
 			Scanner sc = new Scanner (file);
-			while (sc.hasNextLine()) {
-				String [] aux = sc.nextLine().split(":|\\ - ");
 
-				if(aux[0].equals("X-WR-CALNAME")||aux[0].equals("SUMMARY")||aux[0].equals("LOCATION")){
-					System.out.println(aux[1]);
-				}
-				if(aux[0].equals("END")){
-					sc.close();
-					break;
-				}
-				if(aux[0].equals("DTSTART")){
+
+
+			while (sc.hasNextLine()) {
+				String [] aux = sc.nextLine().split(":|\\ - |\\,");
+
+
+
+
+				if(aux[0].equals("DTSTART") && Integer.parseInt(aux[1].substring(0,8))==day){
 					String unOrganizedDate = aux[1];
 					String organizedDate;
 
 					if(unOrganizedDate.length() >8){
 						organizedDate = unOrganizedDate.substring(0,8);
-						String startTime =unOrganizedDate.substring(9,13);
+						aulas.add(organizedDate);
+						int startTime = Integer.parseInt(unOrganizedDate.substring(9,13));
 						System.out.println("data aula: " + organizedDate);
-						System.out.println("hora aula: " +startTime);
+						System.out.println("hora aula: " + (startTime + 100));
+						aulas.add(Integer.toString((startTime+100)));
+						System.out.println("hora fim: " + (startTime + 230));
 					}
-				}
-				if(aux[0].equals("DTEND")){
-					String endTime = aux[1];
+						sc.nextLine();
+						String [] aux1 = sc.nextLine().split(":|\\ - |\\,");
 
-					if(endTime.length() >8){
-						endTime = endTime.substring(9,13);
-						System.out.println("hora fim: " + endTime);
-					}
-				}		
+						System.out.println("meqie " + aux1[0]);
+						if(aux1[0].equals("SUMMARY")){
+							aulas.add(aux1[1]);
+							System.out.println(aux1[1]);
+						}
+						String aux2;
+						while(!(aux2 = sc.nextLine()).split(":")[0].equals("LOCATION")){
+						}
+						
+						if (aux2.split(":|\\ - |\\,")[0].equals("LOCATION")){
+							aulas.add(aux2.split(":|\\ - |\\,")[1]);
+							System.out.println(aux2.split(":|\\ - |\\,")[1]);
+						}
+
+						if(aux1[0].equals("DTEND")){
+							String endTime = aux1[1];
+							
+
+							if(endTime.length() >8){
+								endTime = endTime.substring(9,13);
+								System.out.println("hora fim: " + endTime);
+							}
+						}
+						if(aux1[0].equals("DTSTART")){
+							String endTime = aux1[1];
+
+							if(endTime.length() >8){
+								endTime = endTime.substring(9,13);
+								System.out.println("hora inicio: " + endTime);
+							}
+						}
+						
+							
+				}
 			}
 			sc.close();
 		}
 		catch (FileNotFoundException e) {
 			System.out.println("Não é possivel aceder ao ficheiro");
 		}
+		return aulas;
 	}
 
 
@@ -214,10 +165,9 @@ public class Calendar {
 		System.out.println(abc[1]);
 		System.out.println((Integer.toString(week).equals(abc[1])));
 		System.out.println(d[0].equals("DTSTART"));
-		
-		workWeek("Luis Fraga", 20221010);
-		
-		readDay ("URI_teste.txt", 20221010);
+
+
+		readDay("URI_teste.txt", 20221018);
 
 	}
 
