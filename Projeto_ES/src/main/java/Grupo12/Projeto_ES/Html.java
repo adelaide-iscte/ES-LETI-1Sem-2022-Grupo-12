b5e8ccb134f9faacd1da093dd5f36adaf752e1a1
aadoa <aadoa@iscte-iul.pt>
2022-11-25 14:43:47 +0000

    criaçao de uma classe que permite criar e guardar a informaçao do uri
    dada pelo utilizador. tambem foram feitas algumas alteraçoes na classe
    html para facilitar a manipulaçao com objetos jsons e reduçao de codigo.

--- Projeto_ES/src/main/java/Grupo12/Projeto_ES/Html.java
+++ Projeto_ES/src/main/java/Grupo12/Projeto_ES/Html.java
@@ -1,6 +1,7 @@
 package Grupo12.Projeto_ES;
 
 import java.io.File;
+
 import java.io.PrintWriter;
 
 import org.json.JSONObject;
@@ -13,16 +14,8 @@ public class Html {
 	public static void main(String[] args) {
 		File file = new File("agenda.html");
 		try {
-        String hora = "8:00-9:30";
         int hour = 8;
-        String professor = "fixolas";
-        String sala = "auditorio";
-        String disciplina = "LeagueOfLegends";        
-		int semana = 1;	
-		
-		String bloco ="<td align=\"center\" height=\"100\"><font color=\"blue\">Cadeira: </font>"
-				+ "Engenharia Software<br><font color=\"blue\">Sala:</font> "
-				+ "B206<br><font color=\"blue\">Professor:</font> Atum Tuna</td>\r\n";
+        int day = 20221213;
 		PrintWriter printWriter = new PrintWriter(file);				
 		String html = "<!DOCTYPE html>\r\n"
 				+ "<html>\r\n"
@@ -59,101 +52,89 @@ public class Html {
 				+ "        <tr>\r\n"
 				+ "            <td align=\"center\" height=\"100\">\r\n"
 				+ "                <b>" + hour + ":00-" + (hour+=1) + ":30</b></td>\r\n"
-				+ bloco(hour, null)
-				+ bloco(hour, null)
-				+ bloco(hour, null)
-				+ bloco(hour, null)
-				+ bloco(hour, null)
+				+ bloco(hour,day, null)
+				+ bloco(hour,day, null)
+				+ bloco(hour,day, null)
+				+ bloco(hour,day, null)
+				+ bloco(hour,day, null)
 				+ "        </tr>\r\n"
 				+ "        <tr>\r\n"
 				+ "            <td align=\"center\" height=\"100\">\r\n"
 				+ "                <b>9:30-11:00</b></td>\r\n"
-				+ bloco(hour, null)
-				+ bloco(hour, null)
-				+ bloco(hour, null)
-				+ bloco(hour, null)
-				+ bloco(hour, null)
+				+ bloco(hour,day, null)
+				+ bloco(hour,day, null)
+				+ bloco(hour,day, null)
+				+ bloco(hour,day, null)
+				+ bloco(hour,day, null)
 				+ "        </tr>\r\n"
 				+ "        <tr>\r\n"
 				+ "            <td align=\"center\" height=\"100\">\r\n"
 				+ "                <b>11:00-12:30</b></td>\r\n"
-				+ bloco(hour, null)
-				+ bloco(hour, null)
-				+ bloco(hour, null)
-				+ bloco(hour, null)
-				+ bloco(hour, null)
+				+ bloco(hour,day, null)
+				+ bloco(hour,day, null)
+				+ bloco(hour,day, null)
+				+ bloco(hour,day, null)
+				+ bloco(hour,day, null)
 				+ "        </tr>\r\n"
 				+ "        <tr>\r\n"
-				+ "            <td align=\"center\" height=\"50\">\r\n"
-				+ "                <b>12:30-13:00</b></td>\r\n"
-				+ "            <td align=\"center\" height=\"50\"></td>\r\n"
-				+ "            <td align=\"center\" height=\"50\"></td>\r\n"
-				+ "            <td align=\"center\" height=\"50\"></td>\r\n"
-				+ "            <td align=\"center\" height=\"50\"></td>\r\n"
-				+ "            <td align=\"center\" height=\"50\"></td>\r\n"
+                + blocoVazio(12,13)
 				+ "        </tr>\r\n"
 				+ "        <tr>\r\n"
 				+ "            <td align=\"center\" height=\"100\">\r\n"
 				+ "                <b>13:00-14:30</b></td>\r\n"
-				+ bloco(hour, null)
-				+ bloco(hour, null)
-				+ bloco(hour, null)
-				+ bloco(hour, null)
-				+ bloco(hour, null)
+				+ bloco(hour,day, null)
+				+ bloco(hour,day, null)
+				+ bloco(hour,day, null)
+				+ bloco(hour,day, null)
+				+ bloco(hour,day, null)
 				+ "        </tr>\r\n"
 				+ "        <tr>\r\n"
 				+ "            <td align=\"center\" height=\"100\">\r\n"
 				+ "                <b>14:30-16:00</b></td>\r\n"
-				+ bloco(hour, null)
-				+ bloco(hour, null)
-				+ bloco(hour, null)
-				+ bloco(hour, null)
-				+ bloco(hour, null)
+				+ bloco(hour,day, null)
+				+ bloco(hour,day, null)
+				+ bloco(hour,day, null)
+				+ bloco(hour,day, null)
+				+ bloco(hour,day, null)
 				+ "        </tr>\r\n"
 				+ "        <tr>\r\n"
 				+ "            <td align=\"center\" height=\"100\">\r\n"
 				+ "                <b>16:00-17:30</b></td>\r\n"
-				+ bloco(hour, null)
-				+ bloco(hour, null)
-				+ bloco(hour, null)
-				+ bloco(hour, null)
-				+ bloco(hour, null)
+				+ bloco(hour,day, null)
+				+ bloco(hour,day, null)
+				+ bloco(hour,day, null)
+				+ bloco(hour,day, null)
+				+ bloco(hour,day, null)
 				+ "        </tr>\r\n"
 				+ "        <tr>\r\n"
-				+ "            <td align=\"center\" height=\"50\">\r\n"
-				+ "                <b>17:30-18:00</b></td>\r\n"
-				+ "            <td align=\"center\" height=\"50\"></td>\r\n"
-				+ "            <td align=\"center\" height=\"50\"></td>\r\n"
-				+ "            <td align=\"center\" height=\"50\"></td>\r\n"
-				+ "            <td align=\"center\" height=\"50\"></td>\r\n"
-				+ "            <td align=\"center\" height=\"50\"></td>\r\n"
+                + blocoVazio(17,18)
 				+ "        </tr>\r\n"
 				+ "        <tr>\r\n"
 				+ "            <td align=\"center\" height=\"100\">\r\n"
 				+ "                <b>18:00-19:30</b></td>\r\n"
-				+ bloco(hour, null)
-				+ bloco(hour, null)
-				+ bloco(hour, null)
-				+ bloco(hour, null)
-				+ bloco(hour, null)
+				+ bloco(hour,day, null)
+				+ bloco(hour,day, null)
+				+ bloco(hour,day, null)
+				+ bloco(hour,day, null)
+				+ bloco(hour,day, null)
 				+ "        </tr>\r\n"
 				+ "        <tr>\r\n"
 				+ "            <td align=\"center\" height=\"100\">\r\n"
 				+ "                <b>19:30-21:00</b></td>\r\n"
-				+ bloco(hour, null)
-				+ bloco(hour, null)
-				+ bloco(hour, null)
-				+ bloco(hour, null)
-				+ bloco(hour, null)
+				+ bloco(hour,day, null)
+				+ bloco(hour,day, null)
+				+ bloco(hour,day, null)
+				+ bloco(hour,day, null)
+				+ bloco(hour,day, null)
 				+ "        </tr>\r\n"
 				+ "        <tr>\r\n"
 				+ "            <td align=\"center\" height=\"100\">\r\n"
 				+ "                <b>21:00-22:30</b></td>\r\n"
-				+ bloco(hour, null)
-				+ bloco(hour, null)
-				+ bloco(hour, null)
-				+ bloco(hour, null)
-				+ bloco(hour, null)
+				+ bloco(hour,day, null)
+				+ bloco(hour,day, null)
+				+ bloco(hour,day, null)
+				+ bloco(hour,day, null)
+				+ bloco(hour,day, null)
 				+ "        </tr>\r\n"
 				+ "    </table>\r\n"
 				+ "</body>\r\n"
@@ -181,10 +162,22 @@ public class Html {
 	    	}
 	     file.delete();
 	}
-	
-	public static String bloco(int hour, JSONObject obj) {
+	public static String blocoVazio(int hora1, int hora2) {
+		return  "<td align=\"center\" height=\"50\">\r\n"
+				+ "                <b>" + hora1 +":30-" + hora2 + ":00</b></td>\r\n"
+				+ "            <td align=\"center\" height=\"50\"></td>\r\n"
+				+ "            <td align=\"center\" height=\"50\"></td>\r\n"
+				+ "            <td align=\"center\" height=\"50\"></td>\r\n"
+				+ "            <td align=\"center\" height=\"50\"></td>\r\n"
+				+ "            <td align=\"center\" height=\"50\"></td>\r\n";
 		
+		
+		
+		
+	}
+		public static String bloco(int hour, int day, JSONObject obj) {		
 		String hora = Integer.toString(hour);
+		String dia = Integer.toString(day);
 		
 		
 		return "<td align=\"center\" height=\"100\"><font color=\"blue\">Cadeira: </font>"
@@ -192,7 +185,7 @@ public class Html {
 				+ "B206<br><font color=\"blue\">Professor:</font> Atum Tuna</td>\r\n";
 		
 //		return "<td align=\"center\" height=\"100\"><font color=\"blue\">Cadeira: </font>"
-//		+ ((((JSONObject) obj.get(hora)).get("Cadeira"))) + "<br><font color=\"blue\">Sala:</font> "
+//		+ ((((JSONObject) obj.get(dia).get(hora)).get("Cadeira"))) + "<br><font color=\"blue\">Sala:</font> "
 //		+ "B206<br><font color=\"blue\">Professor:</font> Atum Tuna</td>\r\n";
 	}
 
