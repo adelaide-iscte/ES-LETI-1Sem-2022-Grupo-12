8e2168e20c155d994880176f92d7d7d900e09d32
Luis-ISCTE <luis2002fraga@gmail.com>
2022-11-25 16:22:31 +0000

    Objeto JSON criado

--- Projeto_ES/src/main/java/Grupo12/Projeto_ES/Calendar.java
+++ Projeto_ES/src/main/java/Grupo12/Projeto_ES/Calendar.java
@@ -8,6 +8,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -19,6 +20,8 @@ import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
+import javax.net.ssl.HttpsURLConnection;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -36,11 +39,13 @@ public class Calendar {
 				while (sc.hasNextLine()) {
 					String[] aux = sc.nextLine().split(":");
 					if (aux[1].equals(name)) {
-						return sc.nextLine();
+						String aeiou = sc.nextLine();
+						System.out.println(aeiou);
+						return aeiou;
 					}
 				}
-
 				sc.close();
+
 			} catch (FileNotFoundException e) {
 				System.out.println("Não é possivel aceder ao ficheiro");
 			}
@@ -50,124 +55,175 @@ public class Calendar {
 		return null;
 	}
 
-
-
-
-	
-	public static List<String> readDay (String string, int day){
+	public static List<String> readDay(String string, int day) {
 
 		File file = new File(string);
-		//oioi
 		List<String> aulas = new ArrayList<String>();
-		
+		String organizedDate = null;
+		String disciplina=null;
+		int startTime = 0;
 
-
-		try { 
-			Scanner sc = new Scanner (file);
-
-
+		try {
+			Scanner sc = new Scanner(file);
 
 			while (sc.hasNextLine()) {
-				String [] aux = sc.nextLine().split(":|\\ - |\\,");
+				String[] aux = sc.nextLine().split(":|\\ - |\\,");
 
-
-
-
-				if(aux[0].equals("DTSTART") && Integer.parseInt(aux[1].substring(0,8))==day){
+				if (aux[0].equals("DTSTART") && Integer.parseInt(aux[1].substring(0, 8)) == day) {
 					String unOrganizedDate = aux[1];
-					String organizedDate;
 
-					if(unOrganizedDate.length() >8){
-						organizedDate = unOrganizedDate.substring(0,8);
-						aulas.add(organizedDate);
-						int startTime = Integer.parseInt(unOrganizedDate.substring(9,13));
-						System.out.println("data aula: " + organizedDate);
-						System.out.println("hora aula: " + (startTime + 100));
-						aulas.add(Integer.toString((startTime+100)));
-						System.out.println("hora fim: " + (startTime + 230));
+					if (unOrganizedDate.length() > 8) {
+						organizedDate = unOrganizedDate.substring(0, 8);
+						startTime = Integer.parseInt(unOrganizedDate.substring(9, 13));
 					}
-						sc.nextLine();
-						String [] aux1 = sc.nextLine().split(":|\\ - |\\,");
+					sc.nextLine();
+					String[] disciplinas = sc.nextLine().split(":|\\ - |\\,");
 
-						System.out.println("meqie " + aux1[0]);
-						if(aux1[0].equals("SUMMARY")){
-							aulas.add(aux1[1]);
-							System.out.println(aux1[1]);
-						}
-						String aux2;
-						while(!(aux2 = sc.nextLine()).split(":")[0].equals("LOCATION")){
-						}
-						
-						if (aux2.split(":|\\ - |\\,")[0].equals("LOCATION")){
-							aulas.add(aux2.split(":|\\ - |\\,")[1]);
-							System.out.println(aux2.split(":|\\ - |\\,")[1]);
-						}
+					if (disciplinas[0].equals("SUMMARY")) {
+						disciplina = disciplinas[1];
+					}
+					String sala;
+					while (!(sala = sc.nextLine()).split(":")[0].equals("LOCATION")) {
+					}
 
-						if(aux1[0].equals("DTEND")){
-							String endTime = aux1[1];
-							
-
-							if(endTime.length() >8){
-								endTime = endTime.substring(9,13);
-								System.out.println("hora fim: " + endTime);
-							}
+					if (sala.split(":|\\ - |\\,")[0].equals("LOCATION")) {
+						if (sala.split(":|\\ - |\\,").length > 1) {
+							aulas.add(disciplina);
+							aulas.add(sala.split(":|\\ - |\\,")[1]);
+							aulas.add(Integer.toString((startTime + 100)));
 						}
-						if(aux1[0].equals("DTSTART")){
-							String endTime = aux1[1];
+					}
 
-							if(endTime.length() >8){
-								endTime = endTime.substring(9,13);
-								System.out.println("hora inicio: " + endTime);
-							}
+					if (disciplinas[0].equals("DTEND")) {
+						String endTime = disciplinas[1];
+
+						if (endTime.length() > 8) {
+							endTime = endTime.substring(9, 13);
 						}
-						
-							
+					}
+					if (disciplinas[0].equals("DTSTART")) {
+						String endTime = disciplinas[1];
+
+						if (endTime.length() > 8) {
+							endTime = endTime.substring(9, 13);
+						}
+					}
+
 				}
 			}
 			sc.close();
-		}
-		catch (FileNotFoundException e) {
+		} catch (FileNotFoundException e) {
 			System.out.println("Não é possivel aceder ao ficheiro");
 		}
+		if (organizedDate != null)
+			aulas.add(organizedDate);
 		return aulas;
 	}
 
+	public static void createURIFile(String name, String uri) throws Exception {
+		String fileName = name;
+		File file = new File(fileName + "URI.txt");
+		if (!file.exists()) {
+			try {
+
+				PrintWriter printWriter = new PrintWriter(fileName + "URI.txt");
+				String httpsURL = "https:" + uri;
+				URL myUrl = new URL(httpsURL);
+				HttpsURLConnection conn = (HttpsURLConnection) myUrl.openConnection();
+				InputStream is = conn.getInputStream();
+				InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+				BufferedReader br = new BufferedReader(isr);
+				String inputLine;
+				while ((inputLine = br.readLine()) != null) {
+					printWriter.println(inputLine);
+					// System.out.println(inputLine);
+				}
+				printWriter.close();
+				br.close();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static JSONObject getJsonWeek(String uri, int fistDay) {
+
+		return null;
+	}
 
 	public static void main(String[] args) {
 
-		JSONObject json = new JSONObject();
-		JSONObject dia = new JSONObject();
-		JSONArray jArray = new JSONArray();
+//		JSONObject json = new JSONObject();
+//		JSONObject dia = new JSONObject();
+//		JSONArray jArray = new JSONArray();
+//
+//		dia.put("disciplina", "PCD");
+//		dia.put("horaInicio", "13:00");
+//
+//		jArray.put(2, dia);
+//
+//		json.put("2022/10/10", jArray);
+//		json.put("atum", "tuna");
+//		json.put("20221011", "PCD;1330;C405");
+//		json.put("vvv", dia);
+//
+//		System.out.println(json.get("20221011").toString().split(";")[0]);
+//		System.out.println((((JSONObject) json.get("vvv")).get("disciplina")));
+//
+//		String s = "aa:ss:aa";
+//		String[] f = s.split("#");
+//		System.out.println(f[0]);
+//
+//		String a = "DTSTART:20221010T100000Z";
+//		String[] d = a.split(":");
+//		String[] ab = a.split("T");
+//		String[] abc = ab[3].split(":");
+//		int week = 20221010;
+//
+//		System.out.println(abc[1]);
+//		System.out.println((Integer.toString(week).equals(abc[1])));
+//		System.out.println(d[0].equals("DTSTART"));
 
-		dia.put("disciplina", "PCD");
-		dia.put("horaInicio", "13:00");
+		try {
+			createURIFile("Luis Fraga",
+					"//fenix.iscte-iul.pt/publico/publicPersonICalendar.do?method=iCalendar&username=ldmfa@iscte.pt&password=WO0G8YATTP1tDnHcsadLlOXY7DVzzCFtAWEPoynnnwrrXisaK5XWziFEPrQWRBumQEpCAyhUtOLfEs9Q1qQncKseCMnNMVAf6LZd71erh2tDbygT5lJ9TENQKdiepPlN");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		JSONObject semana = new JSONObject();
+		int inicioSemana = 20221010;
+		int j = 0;
 
-		jArray.put(2, dia);
+		while (j <= 4) {
+			System.out.println(inicioSemana);
+			System.out.println(j);
+			if (readDay("Luis FragaURI.txt", inicioSemana) != null) {
+				List<String> day = new ArrayList<String>(readDay("Luis FragaURI.txt", inicioSemana));
+				JSONObject info = new JSONObject();
+				JSONObject hora = new JSONObject();
+				int i = 0;
+				for (String objects : day) {
+					if (i == 3) {
+						info = new JSONObject();
+						i = 0;
+					}
+					if (i == 0)
+						info.put("Disciplina", objects);
+					if (i == 1)
+						info.put("Sala", objects);
+					if (i == 2)
+						hora.put(objects, info);
 
-		json.put("2022/10/10", jArray);
-		json.put("atum", "tuna");
-		json.put("20221011", "PCD;1330;C405");
-		json.put("vvv", dia);
-
-		System.out.println(json.get("20221011").toString().split(";")[0]);
-		System.out.println((((JSONObject) json.get("vvv")).get("disciplina")));
-
-		String s = "aa:ss:aa";
-		String[] f = s.split("#");
-		System.out.println(f[0]);
-
-		String a = "DTSTART:20221010T100000Z";
-		String[] d = a.split(":");
-		String[] ab = a.split("T");
-		String[] abc = ab[3].split(":");
-		int week = 20221010;
-
-		System.out.println(abc[1]);
-		System.out.println((Integer.toString(week).equals(abc[1])));
-		System.out.println(d[0].equals("DTSTART"));
-
-
-		readDay("URI_teste.txt", 20221018);
+					i++;
+				}
+				if (day.size() > 0)
+					semana.put(day.get(day.size() - 1), hora);
+			}
+			j++;
+			inicioSemana += 1;
+		}
+		System.out.println(semana);
 
 	}
 
