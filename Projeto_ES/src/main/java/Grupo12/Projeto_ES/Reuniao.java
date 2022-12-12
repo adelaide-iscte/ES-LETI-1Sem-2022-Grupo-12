363c00326b4fc799ee3c373230195750f2302aee
Luis-ISCTE <luis2002fraga@gmail.com>
2022-12-12 15:08:10 +0000

    Criação da classe reunião

---
+++ Projeto_ES/src/main/java/Grupo12/Projeto_ES/Reuniao.java
@@ -0,0 +1,34 @@
+package Grupo12.Projeto_ES;
+
+import java.util.ArrayList;
+
+public class Reuniao {
+	
+	private int beginDay;
+	private ArrayList<String> nomes = new ArrayList<String>();
+	private boolean perferencia;
+	private String regularidade;
+	
+	public Reuniao (int beginDay, ArrayList<String> nomes, boolean perferencia, String regularidade) {
+		this.beginDay=beginDay;
+		this.nomes=nomes;
+		this.perferencia=perferencia;
+		this.regularidade=regularidade;
+	}
+	
+	
+	public void gerarReuniao() {
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+
+}
--- Projeto_ES/src/main/java/Grupo12/Projeto_ES/ReuniaoInterface.java
+++ Projeto_ES/src/main/java/Grupo12/Projeto_ES/ReuniaoInterface.java
@@ -16,6 +16,7 @@ import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JRadioButton;
 import javax.swing.WindowConstants;
 
@@ -24,8 +25,10 @@ public class ReuniaoInterface {
 	private JFrame frame;
 	private final int WIDTH = (int) Toolkit.getDefaultToolkit().getScreenSize().getWidth();;
 	private final int HEIGHT = (int) Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+	private ArrayList<JCheckBox> boxes = new ArrayList<JCheckBox>();
+	private ArrayList<String> nomes = new ArrayList<String>();
 	private JComboBox<String> inicioDasReunioes;
-	private JComboBox<String> opcoes;
+	private JComboBox<String> opcoesDeRegularidade;
 	private JRadioButton manha;
 	private JRadioButton tarde;;
 
@@ -36,24 +39,24 @@ public class ReuniaoInterface {
 
 		frame.setSize(500, 400);
 
-		frame.setLocation((WIDTH / 2) - frame.getWidth()-300, (HEIGHT / 2) - frame.getHeight()+200);
+		frame.setLocation((WIDTH / 2) - frame.getWidth() - 300, (HEIGHT / 2) - frame.getHeight() + 200);
 
 		addFrameContents();
 
 	}
-	
+
 	private void addFrameContents() {
-		frame.setLayout(new GridLayout(0,1));
-		
-		frame.add(new JLabel ("Participantes da reunião"));
-		
+		frame.setLayout(new GridLayout(0, 1));
+
+		frame.add(new JLabel("Participantes da reunião"));
+
 		File file = new File("calendarios.txt");
-		
+
 		try {
 			Scanner sc = new Scanner(file);
-			
+
 			List<String> contents = new ArrayList<String>();
-			
+
 			while (sc.hasNextLine()) {
 				String[] aux = sc.nextLine().split(":");
 				if (aux[0].equals("Nome")) {
@@ -61,75 +64,83 @@ public class ReuniaoInterface {
 
 				}
 			}
-			
+
 			sc.close();
 
-			
 			for (String nome : contents) {
 				JCheckBox box = new JCheckBox(nome);
+				boxes.add(box);
 				frame.add(box);
 			}
-			
+
 			manha = new JRadioButton("Manhã");
 			tarde = new JRadioButton("Tarde");
-			
+
 			ButtonGroup group = new ButtonGroup();
 			group.add(manha);
 			group.add(tarde);
-			
+
 			frame.add(new JLabel());
-			
+
 			frame.add(new JLabel("Preferencia na parte do dia"));
 			frame.add(manha);
 			frame.add(tarde);
-			
-			
-			
+
 			frame.add(new JLabel());
-			
-			String [] opcao = {"Nenhuma", "Diária", "Semanal", "Mensal"};
+
+			String[] opcao = { "Nenhuma", "Diária", "Semanal", "Mensal" };
 			frame.add(new JLabel("Regularidade das reuniões"));
-			
-			opcoes = new JComboBox<String>(opcao);
-			frame.add(opcoes);
-			
-			ArrayList<String> inicioReunioes = new ArrayList<String>(); 
-			
-			for (int i=1;i<=2;i++) {
-				for (int j=1;j<=14;j++) {
-					inicioReunioes.add("Semestre" + i + "/Semana " + j);
+
+			opcoesDeRegularidade = new JComboBox<String>(opcao);
+			frame.add(opcoesDeRegularidade);
+
+			ArrayList<String> inicioReunioes = new ArrayList<String>();
+
+			for (int i = 1; i <= 2; i++) {
+				for (int j = 1; j <= 14; j++) {
+					inicioReunioes.add("Semestre" + i + "/Semana" + j);
 				}
 			}
-			
-			frame.add(new JLabel ("Semana em que começam as reuniões"));
-			
-			String [] comecoReuniao = new String[inicioReunioes.size()+2];
-			
-			for (int i=0;i<inicioReunioes.size();i++) {
-				comecoReuniao[i]=inicioReunioes.get(i);
+
+			frame.add(new JLabel("Semana em que começam as reuniões"));
+
+			String[] comecoReuniao = new String[inicioReunioes.size() + 2];
+
+			for (int i = 0; i < inicioReunioes.size(); i++) {
+				comecoReuniao[i] = inicioReunioes.get(i);
 			}
-			
-			comecoReuniao[inicioReunioes.size()]= "Semestre2/Semana15";
-			comecoReuniao[inicioReunioes.size()+1]= "Semestre2/Semana16";
-			
+
+			comecoReuniao[inicioReunioes.size()] = "Semestre2/Semana15";
+			comecoReuniao[inicioReunioes.size() + 1] = "Semestre2/Semana16";
+
 			inicioDasReunioes = new JComboBox<String>(comecoReuniao);
 			frame.add(inicioDasReunioes);
-			
-			
-			
+
 			frame.add(new JLabel());
-			
+
 			JButton ok = new JButton("OK");
 			ok.addActionListener(new ActionListener() {
 
 				@Override
 				public void actionPerformed(ActionEvent e) {
-					frame.dispose();
+					if (!tarde.isSelected() && !manha.isSelected()) {
+						JOptionPane.showMessageDialog(frame, "Prencha todos os campos");
+
+					} else {
+						participantes();
+						if (nomes.size() == 0) {
+							JOptionPane.showMessageDialog(frame, "Prencha todos os campos");
+
+						} else {
+							criarReuniao();
+							frame.dispose();
+						}
+					}
 				}
 			});
-			
+
 			frame.add(ok);
-			
+
 			JButton cancelar = new JButton("Cancelar");
 			cancelar.addActionListener(new ActionListener() {
 
@@ -140,20 +151,61 @@ public class ReuniaoInterface {
 				}
 			});
 			frame.add(cancelar);
-			
-			
-			
+
 		} catch (FileNotFoundException e) {
 			System.out.println("O ficheiro não foi econtrado");
 		}
-			
+
+	}
+
+	private void criarReuniao() {
+		String [] semestreSemana = ((String) inicioDasReunioes.getSelectedItem()).split("Semestre|\\/|\\Semana");
+		int dia = getDia(semestreSemana);
+		boolean perferencia = getPerferencia();
+		String regularidade = (String) opcoesDeRegularidade.getSelectedItem();
+		
+		Reuniao reuniao = new Reuniao(dia,nomes,perferencia,regularidade);
+		
+
 	}
 	
 	
+	private int getDia(String [] semestreSemana) {
+		int dia=0;
+		
+		if (Integer.parseInt(semestreSemana[1])==1)
+			dia = 20220912;
+		if (Integer.parseInt(semestreSemana[1])==2)
+			dia = 20230206;
+		
+		int semana = Integer.parseInt(semestreSemana[3]);
+		
+		for (int i=0;i<semana-1;i++) {
+			int semanaAnterior=dia;
+			dia=Calendar.nextWeek(semanaAnterior);
+		}
+		
+		
+		return dia;
+	}
 	
+
+	private void participantes() {
+		for (JCheckBox box : boxes) {
+			if (box.isSelected()) {
+				nomes.add(box.getText());
+			}
+		}
+	}
 	
+	private boolean getPerferencia() {
+		if (manha.isSelected())
+			return true;
+		
+		return false;
+	}
 	
-	
+
 	public void open() {
 		frame.setVisible(true);
 	}
